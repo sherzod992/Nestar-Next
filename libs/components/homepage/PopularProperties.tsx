9a588693d2cf87a8f6d4f9f6e320f37885838532
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-07-11 15:01:47 +0900

    fix: modify popular property component

--- libs/components/homepage/PopularProperties.tsx
+++ libs/components/homepage/PopularProperties.tsx
@@ -10,6 +10,11 @@ import { Property } from '../../types/property/property';
 import Link from 'next/link';
 import { PropertiesInquiry } from '../../types/property/property.input';
 
+
+import { GET_PROPERTIES } from '../../../apollo/user/query';
+import { useQuery } from '@apollo/client';
+import { T } from '../../types/common';
+
 interface PopularPropertiesProps {
 	initialInput: PropertiesInquiry;
 }
@@ -20,6 +25,21 @@ const PopularProperties = (props: PopularPropertiesProps) => {
 	const [popularProperties, setPopularProperties] = useState<Property[]>([]);
 
 	/** APOLLO REQUESTS **/
+	const {
+		loading: getPropertiesLoading,
+		data: getPropertiesData,
+		error: getPropertiesError,
+		refetch: getPropertiesRefetch,
+	} = useQuery(GET_PROPERTIES, {
+		fetchPolicy: 'cache-and-network',
+		variables: { input: initialInput },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			setPopularProperties(data?.getProperties?.list);
+		},
+	});
+
+
 	/** HANDLERS **/
 
 	if (!popularProperties) return null;
